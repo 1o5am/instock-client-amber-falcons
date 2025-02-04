ae2fddf8c2b6a8b4b28a38d4964a758588333e56
lovedatsnow <megan.veldhuis@gmail.com>
2025-02-04 13:00:03 -0700

    start styling searchbar

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,6 +1,12 @@
 import "./Searchbar.scss";
+import searchIcon from "../../assets/icons/search-24px.svg";
 function Searchbar() {
-  return <button className="searchbar">Search...</button>;
+  return (
+    <>
+      <input type="text" className="searchbar" placeholder="Search..."></input>
+      <img className="searchbar__icon" src={searchIcon} />
+    </>
+  );
 }
 
 export default Searchbar;
