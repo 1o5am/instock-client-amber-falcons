8be7442402421c96bd92217c8bd1424f9de7737e
hsiangyichen <hsiangyi1025@gmail.com>
2025-02-05 10:16:23 -0800

    feat: Using Navlink for header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.scss";
 import Header from "./components/Header/Header.jsx";
-import HomePage from "./pages/HomePage/HomePage.jsx";
 import WarehousePage from "./pages/WarehousePage/WarehousePage.jsx";
 import InventoryPage from "./pages/InventoryPage/InventoryPage.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -10,8 +9,7 @@ function App() {
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<HomePage />}></Route>
-        <Route path="/warehouse" element={<WarehousePage />}></Route>
+        <Route path="/" element={<WarehousePage />}></Route>
         <Route path="/inventory" element={<InventoryPage />}></Route>
       </Routes>
     </BrowserRouter>
