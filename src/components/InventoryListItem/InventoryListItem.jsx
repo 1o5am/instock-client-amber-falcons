80f0becf34ad8b1255e347d14fc74a0d9866252e
lovedatsnow <megan.veldhuis@gmail.com>
2025-02-05 09:51:44 -0700

    add all info to inventory list items

--- src/components/InventoryListItem/InventoryListItem.jsx
+++ src/components/InventoryListItem/InventoryListItem.jsx
@@ -1,10 +1,32 @@
+import deleteIcon from "../../assets/icons/delete_outline-24px.svg";
+import editIcon from "../../assets/icons/edit-24px.svg";
+
 function InventoryListItem({ item }) {
-  //   console.log(item);
   return (
     <li className="inventory-item__item">
       <div className="inventory-item__info">
-        <p>INVENTORY ITEM</p>
-        <p>{item.item_name}</p>
+        <p className="inventory-item__title">INVENTORY ITEM</p>
+        <p className="inventory-item__name">{item.item_name}</p>
+      </div>
+      <div className="inventory-item__info">
+        <p className="inventory-item__title">CATEGORY</p>
+        <p className="inventory-item__name">{item.category}</p>
+      </div>
+      <div className="inventory-item__info">
+        <p className="inventory-item__title">STATUS</p>
+        <p className="inventory-item__name">{item.status}</p>
+      </div>
+      <div className="inventory-item__info">
+        <p className="inventory-item__title">QUANTITY</p>
+        <p className="inventory-item__name">{item.quantity}</p>
+      </div>
+      <div className="inventory-item__info">
+        <p className="inventory-item__title">WAREHOUSE</p>
+        <p className="inventory-item__name">{item.warehouse_name}</p>
+      </div>
+      <div className="inventory-item__icons">
+        <img className="icon" src={deleteIcon}></img>
+        <img className="icon" src={editIcon}></img>
       </div>
     </li>
   );
