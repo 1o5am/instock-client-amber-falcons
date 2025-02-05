9a6023818c2bbf9bd4e1bf6e44e73ae5270a5eab
lovedatsnow <megan.veldhuis@gmail.com>
2025-02-05 10:55:22 -0700

    add page-content styling

--- src/pages/InventoryHomePage/InventoryHomePage.jsx
+++ src/pages/InventoryHomePage/InventoryHomePage.jsx
@@ -1,8 +1,9 @@
 import InventoryList from "../../components/InventoryList/InventoryList.jsx";
 import Searchbar from "../../components/Searchbar/Searchbar.jsx";
+
 function InventoryHomePage() {
   return (
-    <div>
+    <div className="page-content">
       <h1>Inventory</h1>
       <Searchbar />
       <button>+ Add New Item</button>
