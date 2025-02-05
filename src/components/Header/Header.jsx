09129af7709876008cbe8550bb76f520159fff74
VijayMJ <vijaymj42@gmail.com>
2025-02-04 21:46:55 -0400

    made header component responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,15 @@
-// import instockLogo from "../../assets/Logo/InStock-Logo.svg";
+// import instockLogo from "../../assets/logo/InStock-Logo.svg";
 import instockLogo from "../../assets/logo/InStock-Logo_1x.png";
+import { useState, useEffect } from "react";
 
-import { useLocation, Link } from "react-router-dom";
+import { useLocation, Link, useParams } from "react-router-dom";
 import "./Header.scss";
 
 import React from "react";
 
-function Header() {
+function Header({ page }) {
+  const { location } = useParams();
+
   return (
     <header className="header">
       <section className="header__logo-box">
@@ -15,12 +18,26 @@ function Header() {
           src={instockLogo}
           alt="instock logo"
         />
-        <div className="header__link">
-          <Link to="/">
-            <h3 className="header__warehouses-link">Warehouses</h3>
+        <div className="header__link-content">
+          <Link
+            to="/warehouse"
+            className={`header__link ${
+              page === "warehouse"
+                ? "header__link--active"
+                : "header__link--inactive"
+            }`}
+          >
+            Warehouses
           </Link>
-          <Link to="/">
-            <h3 className="header__inventory-link">Inventory</h3>
+          <Link
+            to="/inventory"
+            className={`header__link ${
+              page === "inventory"
+                ? "header__link--active"
+                : "header__link--inactive"
+            }`}
+          >
+            Inventory
           </Link>
         </div>
       </section>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,10 @@
+import Header from "../../components/Header/Header";
 import "./HomePage.scss";
 
 const HomePage = () => {
   return (
     <>
+      <Header page={"warehouse"} />
       <h2>Home Page</h2>
     </>
   );
