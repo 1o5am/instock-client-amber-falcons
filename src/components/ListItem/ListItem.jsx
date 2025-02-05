3ceb870cab0b28868ab0d08fdf92d98b43ed529e
lovedatsnow <megan.veldhuis@gmail.com>
2025-02-05 14:37:31 -0700

    add links

--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -6,48 +6,50 @@ import "./ListItem.scss";
 
 function ListItem({ item }) {
   return (
-    <Link to={`${item.id}`}>
-      <li className="item">
-        <div className="item__info">
-          <p className="item__title">INVENTORY ITEM</p>
+    <li className="item">
+      <div className="item__info">
+        <p className="item__title">INVENTORY ITEM</p>
+        <Link to={`${item.id}`}>
           <p className="item__name item__name--bold item__name--blue">
             {item.item_name} <img className="icon" src={arrowIcon}></img>
           </p>
-        </div>
-        <div className="item__info item__info--tablet">
-          <p className="item__title">CATEGORY</p>
-          <p className="item__name">{item.category}</p>
-        </div>
-        <div className="item__info">
-          <p className="item__title">STATUS</p>
-          <p
-            className={`item__name item__name--bold item__name--bubble ${
-              item.status === "In Stock"
-                ? "item__name--green"
-                : "item__name--red"
-            }`}
-          >
-            {item.status.toUpperCase()}
-          </p>
-        </div>
-        <div className="item__info item__info--mobile">
-          <p className="item__title">CATEGORY</p>
-          <p className="item__name">{item.category}</p>
-        </div>
-        <div className="item__info item__info--small">
-          <p className="item__title">QTY</p>
-          <p className="item__name">{item.quantity}</p>
-        </div>
-        <div className="item__info item__info--right">
-          <p className="item__title">WAREHOUSE</p>
-          <p className="item__name">{item.warehouse_name}</p>
-        </div>
-        <div className="item__icons">
+        </Link>
+      </div>
+      <div className="item__info item__info--tablet">
+        <p className="item__title">CATEGORY</p>
+        <p className="item__name">{item.category}</p>
+      </div>
+      <div className="item__info">
+        <p className="item__title">STATUS</p>
+        <p
+          className={`item__name item__name--bold item__name--bubble ${
+            item.status === "In Stock" ? "item__name--green" : "item__name--red"
+          }`}
+        >
+          {item.status.toUpperCase()}
+        </p>
+      </div>
+      <div className="item__info item__info--mobile">
+        <p className="item__title">CATEGORY</p>
+        <p className="item__name">{item.category}</p>
+      </div>
+      <div className="item__info item__info--small">
+        <p className="item__title">QTY</p>
+        <p className="item__name">{item.quantity}</p>
+      </div>
+      <div className="item__info item__info--right">
+        <p className="item__title">WAREHOUSE</p>
+        <p className="item__name">{item.warehouse_name}</p>
+      </div>
+      <div className="item__icons">
+        <Link to={`delete/${item.id}`}>
           <img className="icon" src={deleteIcon}></img>
+        </Link>
+        <Link to={`edit/${item.id}`}>
           <img className="icon" src={editIcon}></img>
-        </div>
-      </li>
-    </Link>
+        </Link>
+      </div>
+    </li>
   );
 }
 export default ListItem;
