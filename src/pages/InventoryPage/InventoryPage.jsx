aaadceae92907a60bc8309b60addbdf41b0083b5
lovedatsnow <megan.veldhuis@gmail.com>
2025-02-05 15:44:32 -0700

    add default button classes

--- src/pages/InventoryPage/InventoryPage.jsx
+++ src/pages/InventoryPage/InventoryPage.jsx
@@ -1,15 +1,24 @@
 import InventoryList from "../../components/InventoryList/InventoryList.jsx";
 import Searchbar from "../../components/Searchbar/Searchbar.jsx";
 import "./InventoryPage.scss";
+import { useNavigate } from "react-router-dom";
 
 function InventoryPage() {
+  const navigate = useNavigate();
   return (
     <div className="page-content inventory-page">
       <div className="inventory-page__header">
         <h1 className="inventory-page__title">Inventory</h1>
         <div className="inventory-page__buttons">
           <Searchbar />
-          <button className="inventory-page__add">+ Add New Item</button>
+          <button
+            onClick={() => {
+              navigate("/inventory/add");
+            }}
+            className="btn btn--primary" //inventory-page__add
+          >
+            + Add New Item
+          </button>
         </div>
       </div>
 
