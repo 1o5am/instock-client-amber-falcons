9de49137ba2451dcb0ef4c5a07b0367399ca2bc6
hsiangyichen <hsiangyi1025@gmail.com>
2025-02-05 15:00:26 -0800

    add footer component and made responsive

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,43 @@
+import instockLogo from "../../assets/logo/InStock-Logo.svg";
+import { NavLink, Link } from "react-router-dom";
+import "./Header.scss";
+
+function Header() {
+  return (
+    <header className="header">
+      <section className="header__logo-box">
+        <Link className="header__link-logo" to="/">
+          <img
+            className="header__logo-image"
+            src={instockLogo}
+            alt="instock logo"
+          />
+        </Link>
+        <div className="header__link-content">
+          <NavLink
+            to="/warehouses"
+            className={({ isActive }) =>
+              `header__link ${
+                isActive ? "header__link--active" : "header__link--inactive"
+              }`
+            }
+          >
+            Warehouses
+          </NavLink>
+          <NavLink
+            to="/inventory"
+            className={({ isActive }) =>
+              `header__link ${
+                isActive ? "header__link--active" : "header__link--inactive"
+              }`
+            }
+          >
+            Inventory
+          </NavLink>
+        </div>
+      </section>
+    </header>
+  );
+}
+
+export default Header;
