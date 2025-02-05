e5843c14054e60431c65cdf1037961790831f697
lovedatsnow <megan.veldhuis@gmail.com>
2025-02-05 10:31:08 -0700

    styling

--- src/components/InventoryListItem/InventoryListItem.jsx
+++ src/components/InventoryListItem/InventoryListItem.jsx
@@ -1,12 +1,16 @@
 import deleteIcon from "../../assets/icons/delete_outline-24px.svg";
 import editIcon from "../../assets/icons/edit-24px.svg";
+import arrowIcon from "../../assets/icons/chevron_right-24px.svg";
+import "./InventoryListItem.scss";
 
 function InventoryListItem({ item }) {
   return (
     <li className="inventory-item__item">
       <div className="inventory-item__info">
         <p className="inventory-item__title">INVENTORY ITEM</p>
-        <p className="inventory-item__name">{item.item_name}</p>
+        <p className="inventory-item__name inventory-item__name--blue">
+          {item.item_name} <img src={arrowIcon}></img>
+        </p>
       </div>
       <div className="inventory-item__info">
         <p className="inventory-item__title">CATEGORY</p>
