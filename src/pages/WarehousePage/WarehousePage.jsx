c013a210bd54683cfcdb8246880c948544f369b0
hsiangyichen <hsiangyi1025@gmail.com>
2025-02-06 08:49:47 -0800

    feat: Add warehouse list

--- src/pages/WarehousePage/WarehousePage.jsx
+++ src/pages/WarehousePage/WarehousePage.jsx
@@ -1,5 +1,30 @@
+import WarehouseLists from "../../components/WarehouseLists/WarehouseLists.jsx";
+import Searchbar from "../../components/Searchbar/Searchbar.jsx";
+import "./WarehousePage.scss";
+// import { useNavigate } from "react-router-dom";
+
 const WarehousePage = () => {
-  return <div>WarehousePage</div>;
+  // const navigate = useNavigate();
+  return (
+    <div className="page-content warehouse-page">
+      <div className="warehouse-page__header">
+        <h1 className="warehouse-page__title">Warehouses</h1>
+        <div className="warehouse-page__buttons">
+          <Searchbar />
+          <button
+            // onClick={() => {
+            //   navigate("/inventory/add");
+            // }}
+            className="btn btn--primary warehouse-page__add"
+          >
+            + Add New Item
+          </button>
+        </div>
+      </div>
+
+      <WarehouseLists />
+    </div>
+  );
 };
 
 export default WarehousePage;
